910ad95b16242b51a60268b57c4c71f55850d414
guruGit008 <gurutirangaaerospace@gmail.com>
2025-10-24 16:39:24 +0530

    api.ts upadte

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,5 @@
-export const APIURL = 'https://dev.tirangaidms.com';
+// export const APIURL = 'https://dev.tirangaidms.com';
+
+// Detect if running locally or on server
+export const APIURL =
+  window.location.hostname === 'localhost'? 'http://localhost:8080' : 'https://dev.tirangaidms.com';
