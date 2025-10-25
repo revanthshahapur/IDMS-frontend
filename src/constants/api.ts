3e2ae8ea7b608fc9aba16f87c417c0af75187ba7
guruGit008 <gurutirangaaerospace@gmail.com>
2025-10-25 15:51:38 +0530

    Configured environment variable for production API URL

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,6 @@
 // export const APIURL = 'https://dev.tirangaidms.com';
 
-export const APIURL = process.env.NEXT_PUBLIC_API_URL as string;
+// export const APIURL = process.env.NEXT_PUBLIC_API_URL as string;
+
+
+export const APIURL = process.env.NEXT_PUBLIC_API_URL || 'https://dev.tirangaidms.com';
